8655901e631d584b604c59b683f2d994a5bd94b4
Siddesh <siddeshsankhya@gmail.com>
2023-12-18 15:37:24 +0530

    add: init chat req between peers

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import {useEffect, useState} from "react";
 import {ConnectButton} from "@rainbow-me/rainbowkit";
 import {useAccount} from "wagmi";
 import {PushAPI} from "@pushprotocol/restapi";
-import {useWalletClient} from "wagmi";
+import {} from "wagmi";
+import {ethers} from "ethers";
+
 const socket = io.connect("http://localhost:3001");
 
 function App() {
@@ -18,23 +20,30 @@ function App() {
     socket.emit("connect_to_peer", walletAddress);
   };
 
-  const {data: walletClient} = useWalletClient();
-  const sendChatRequest = async () => {
+  // const provider = new ethers.providers.JsonRpcProvider(
+  //   "https://eth-sepolia.g.alchemy.com/v2/bDT1MTPLZ4EgJq4kQmdY1cJjzhF4MCY5"
+  // );
+  // const signer = provider.getSigner();
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  const sendChatRequest = async (peerAddress) => {
     console.log("clickeed");
-    const userAlice = await PushAPI.initialize(walletClient, {env: "staging"});
+    console.log(peerAddress);
+    const userAlice = await PushAPI.initialize(signer, {env: "staging"});
 
     // Send a message to Bob
-    const aliceMessagesBob = await userAlice.chat.send(peerWalletAddress, {
+    const aliceMessagesBob = await userAlice.chat.send(peerAddress, {
       content: "Gm gm! It's a me... Mario",
     });
     console.log(aliceMessagesBob);
   };
   useEffect(() => {
-    console.log(walletClient);
-    sendChatRequest();
     socket.on("peer_connected", (peerAddress) => {
+      console.log(peerAddress);
       setIsPeerConnected(true);
       setPeerWalletAddress(peerAddress);
+      sendChatRequest(peerAddress);
     });
 
     socket.on("peer_disconnected", () => {
